2c57e79b0d44fe2cce653bd075c93445e9cadfdf
Arseny Bochkarev <bochkarevarseny@gmail.com>
2024-12-07 19:50:18 +0300

    Add part1

--- src/Part1/Tasks.hs
+++ src/Part1/Tasks.hs
@@ -1,6 +1,7 @@
 module Part1.Tasks where
 
 import Util(notImplementedYet)
+import Data.Fixed(mod')
 
 factorial :: Integer -> Integer
 factorial 0 = 1
@@ -19,15 +20,20 @@ myPowDouble x 1 = x
 myPowDouble x y = x * myPowDouble x (y - 1)
 
 -- синус числа (формула Тейлора)
+mySinHelper :: Double -> Integer -> Double
+mySinHelper x n = sum [(myPowDouble (-1) k) * (myPowDouble x (2 * k + 1)) / fromIntegral (factorial (2 * k + 1)) | k <- [0..n]]
+
 mySin :: Double -> Double
-mySin = notImplementedYet
+mySin x = mySinHelper (mod' x (2 * pi)) 40
+-- mySin = notImplementedYet
 
 -- косинус числа (формула Тейлора)
--- myCosHelper :: Double -> Integer -> Double
--- myCosHelper x n = sum [fromIntegral ((myPow (-1) k) * (myPowDouble x (2*k))) / fromIntegral (factorial (2*k)) | k <- [0..n]]
+myCosHelper :: Double -> Integer -> Double
+myCosHelper x n = sum [(myPowDouble (-1) k) * ((myPowDouble x (2 * k)) / fromIntegral (factorial (2 * k))) | k <- [0..n]]
 
 myCos :: Double -> Double
-myCos x = notImplementedYet
+myCos x = myCosHelper (mod' x (2 * pi)) 20
+-- myCos = notImplementedYet
 
 -- наибольший общий делитель двух чисел
 myGCD :: Integer -> Integer -> Integer
@@ -38,8 +44,16 @@ myGCD a b = if b == 0 then abs a else myGCD (abs b) (a `mod` b)
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
+daysInMonth :: Integer -> Integer -> Integer
+daysInMonth year month
+    | month == 2 = if isLeapYear year then 29 else 28
+    | month `elem` [4, 6, 9, 11] = 30
+    | otherwise = 31
+    where
+        isLeapYear year = (year `mod` 4 == 0 && year `mod` 100 /= 0) || (year `mod` 400 == 0)
+
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect = notImplementedYet
+isDateCorrect day month year = year >= 0 && month >= 1 && month <= 12 && day >= 1 && day <= (daysInMonth year month)
 
 -- является ли данное число простым?
 isPrimeHelper :: Integer -> Integer -> Bool
@@ -55,8 +69,7 @@ type Point2D = (Double, Double)
 -- рассчитайте площадь многоугольника по формуле Гаусса
 -- многоугольник задан списком координат
 shapeArea :: [Point2D] -> Double
---shapeArea points = notImplementedYet
-shapeArea = notImplementedYet
+shapeArea points = 0.5 * abs (sum [(x1 * y2) - (x2 * y1) | ((x1, y1), (x2, y2)) <- zip points (tail points ++ [head points])])
 
 -- треугольник задан длиной трёх своих сторон.
 -- функция должна вернуть
@@ -64,5 +77,25 @@ shapeArea = notImplementedYet
 --  1, если он остроугольный
 --  2, если он прямоугольный
 --  -1, если это не треугольник
+isTriangle :: Double -> Double -> Double -> Bool
+isTriangle x y z = x + y > z && x + z > y && y + z > x
+
 triangleKind :: Double -> Double -> Double -> Integer
-triangleKind a b c = notImplementedYet
+triangleKind a b c
+    | a <= 0 || b <= 0 || c <= 0 = -1  -- Если одна из сторон неположительна
+    | not (isTriangle a b c) = -1       -- Если стороны не могут образовать треугольник
+    -----------------------------------------
+    -- Прямоугольный? Теорема Пифагора
+    | a * a + b * b == c * c = 2
+    | a * a + c * c == b * b = 2
+    | a * a  == c * c + b * b = 2
+    -----------------------------------------
+    -- Тупоугольный? Сумма квадратов двух меньших стороны меньше квадрата большей стороны
+    -- a -- наибольшая сторона
+    | a > b && a > c && a * a > b * b + c * c = 0
+    -- b -- наибольшая сторона
+    | b > a && b > c && b * b > a * a + c * c = 0
+    -- c -- наибольшая сторона
+    | c > a && c > b && c * c > a * a + b * b = 0
+    -----------------------------------------
+    | otherwise = 1 -- Остроугольный
